33fd1c7db2534b345296a580b33890200a6c7366
“Deena <deenah.kamal@gmail.com>
2023-06-26 18:48:07 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _putchar - writes the character c to stdout
+ *
+ *
+ * @c: character to print
+ *
+ * Return: on success 1.
+ * on wrror, -1 is returned
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
